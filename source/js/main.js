e63e8776e9c4026c387ad1dfc1e75aeaf274a55c
alexandra-tsidulko <alexandra.tsidulko@gmail.com>
2023-04-12 17:24:53 +0700

    Раздел Абонементы - десктопная версия

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initVideos} from './modules/video';
+import {initTabs} from './vendor/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -23,6 +24,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
     initVideos();
+    initTabs();
   });
 });
 
