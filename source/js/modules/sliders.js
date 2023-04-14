7df1262945d39cfdc1f2405824ede685880cea84
alexandra-tsidulko <alexandra.tsidulko@gmail.com>
2023-04-14 22:55:34 +0700

    секция отзывы

--- source/js/modules/sliders.js
+++ source/js/modules/sliders.js
@@ -25,8 +25,23 @@ const initSliders = () => {
       },
       // Navigation arrows
       navigation: {
-        nextEl: '.swiper-button-next',
+        nextEl: '.staff__slider-button--next',
         prevEl: '.staff__slider-button--prev',
+      },
+    });
+  }
+
+  if (document.querySelector('.reviews__slider')) {
+    const reviewsSlider = new Swiper('.reviews__slider', {
+      direction: 'horizontal',
+      loop: false,
+      slidesPerView: 'auto',
+      spaceBetween: 60,
+      autoHeight: true,
+
+      navigation: {
+        nextEl: '.reviews__button--next',
+        prevEl: '.reviews__button--prev',
       },
     });
   }
