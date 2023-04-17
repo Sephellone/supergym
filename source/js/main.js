ae1420f7d86b406c0d65a41e971e175fec40a5b4
alexandra-tsidulko <alexandra.tsidulko@gmail.com>
2023-04-17 22:39:50 +0700

    шаманство с таймаутом

--- source/js/main.js
+++ source/js/main.js
@@ -18,8 +18,10 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initYTapi();
-    initVideoButton();
+    setTimeout(() => {
+      initYTapi();
+      initVideoButton();
+    }, 1000);
     initTabs();
     initSliders();
   });
