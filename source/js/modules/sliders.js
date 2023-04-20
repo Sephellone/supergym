a82b9a465fc05f0aa1bad16f5186b19a6097664e
alexandra-tsidulko <alexandra.tsidulko@gmail.com>
2023-04-20 15:24:54 +0700

    костыли для свайпера

--- source/js/modules/sliders.js
+++ source/js/modules/sliders.js
@@ -1,5 +1,14 @@
+const staffSliderElement = document.querySelector('.staff__slider');
+
+function addTabindex() {
+  const originalSlides = document.querySelectorAll('.staff__item:not(.swiper-slide-duplicate)');
+  for (let i = 0; i < originalSlides.length; i++) {
+    originalSlides[i].querySelector('.staff-card').setAttribute('tabindex', '0');
+  }
+}
+
 const initSliders = () => {
-  if (document.querySelector('.staff__slider')) {
+  if (staffSliderElement) {
     const staffSlider = new Swiper('.staff__slider', {
       direction: 'horizontal',
       loop: true,
@@ -27,16 +36,34 @@ const initSliders = () => {
           centeredSlides: false,
         },
       },
+
+      on: {
+        init: () => {
+          addTabindex();
+          document.addEventListener('keydown', (evt) => {
+
+            if (evt.shiftKey && evt.key === 'Tab') {
+              const active = document.activeElement;
+              if (active.parentElement.classList.contains('swiper-slide-active')) {
+                const parentSlideIndex = active.parentElement.dataset.swiperSlideIndex;
+                if (parentSlideIndex > 0) {
+                  staffSlider.slidePrev(100, true);
+                }
+              }
+            } else if (!evt.shiftKey && evt.key === 'Tab') {
+              const active = document.activeElement;
+              if (active.parentElement.classList.contains('swiper-slide-next')) {
+                staffSlider.slideNext(100, true);
+              }
+            }
+          });
+        },
+      },
       // Navigation arrows
       navigation: {
         nextEl: '.staff__slider-button--next',
         prevEl: '.staff__slider-button--prev',
       },
-    });
-
-    const duplicates = document.querySelectorAll('.swiper-slide-duplicate');
-    duplicates.forEach((duplicate) => {
-      duplicate.querySelector('.staff-card').setAttribute('tabindex', '-1');
     });
   }
 
